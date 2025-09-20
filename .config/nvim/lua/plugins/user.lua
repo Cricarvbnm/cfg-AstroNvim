fe59dcf5638dd63dca165e983dfa780c208afebd
Alec <alecvbnm@outlook.com>
2025-09-20 18:14:34 +0800

    sync

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -11,4 +11,19 @@ return {
     ft = { "markdown" },
     build = ":call mkdp#util#install()",
   },
+
+  -- {
+  --   "lervag/vimtex",
+  --   lazy = false, -- we don't want to lazy load VimTeX
+  --   init = function() vim.g.vimtex_view_method = "zathura" end,
+  --   config = function()
+  --     vim.g.tex_flavor = "latex"
+  --
+  --     vim.g.vimtex_compiler_latexmk_engines = { _ = "-xelatex" }
+  --     vim.g.vimtex_compiler_latexrun_engines = { _ = "xelatex" }
+  --
+  --     vim.g.vimtex_complete_enabled = 0
+  --     vim.g.vimtex_syntax_enabled = 0
+  --   end,
+  -- },
 }
